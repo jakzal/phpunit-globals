124e5232c5c743454857fc094e729fdf53a8a586
Franz Zieris <franz.zieris@gmail.com>
2020-06-04 12:03:00 +0200

    Add testcase to test handling of PHPUnit warnings

---
+++ tests/AnnotationExtensionWithWarningTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Zalas\PHPUnit\Globals\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AnnotationExtensionWithWarningTest extends TestCase
+{
+    /**
+     * @dataProvider warning_producing_provider
+     */
+    public function test_it_handles_warnings()
+    {
+        $this->assertTrue(true, 'It lets the test cases run normally');
+    }
+
+    /**
+     * This provider intentionally produces a PHPUnit warning by using the same
+     * key twice
+     */
+    public function warning_producing_provider()
+    {
+        yield 'test' => [1];
+        yield 'test' => [2];
+    }
+}
